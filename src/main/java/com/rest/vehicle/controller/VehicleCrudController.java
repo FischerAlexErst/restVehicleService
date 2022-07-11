9c971b1541a50c005d96d416e9ce57db94ec8f58
arybakov <arybakov@iflex.ru>
2022-07-11 18:27:49 +0200

    in API was added controller with swagger description

---
+++ src/main/java/com/rest/vehicle/controller/VehicleCrudController.java
@@ -0,0 +1,131 @@
+package com.rest.vehicle.controller;
+
+import com.rest.vehicle.dto.VehicleDto;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Handler;
+
+@RestController
+@RequestMapping("/api/vehicles")
+@Tag(name="Vehicle", description="CRUD-a controller that works with vehicles")
+public class VehicleCrudController {
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "successful receipt list of the vehicles",
+                    content = {
+                            @Content(schema = @Schema(implementation = Page.class))
+                    }
+            ),
+            @ApiResponse(responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content
+            )}
+    )
+    @GetMapping(params = {"page", "size", "sort"}, produces= {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Page<VehicleDto>> findAll(@RequestParam(value = "page") Integer page,
+                                                    @RequestParam(value = "size") Integer size,
+                                                    @RequestParam(value = "sort") String sort) {
+        return null;
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "successful receipt of the vehicle",
+                    content = {
+                            @Content(schema = @Schema(implementation = VehicleDto.class))
+                    }
+            ),
+            @ApiResponse(responseCode = "404",
+                    description = "vehicle not found",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content
+            )}
+    )
+    @GetMapping(value = "/{id}", produces= {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<VehicleDto> getVehicleById(@PathVariable("id") String id) {
+        return null;
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "successful created the vehicle",
+                    content = {
+                            @Content(schema = @Schema(implementation = VehicleDto.class))
+                    }
+            ),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid input",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "409",
+                    description = "Vehicle already exists",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content
+            )}
+    )
+    @PostMapping(produces= {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<VehicleDto> createVehicle(@Valid @RequestBody VehicleDto vehicleDto) {
+        return null;
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "successful operation",
+                    content = {
+                            @Content(schema = @Schema(implementation = VehicleDto.class))
+                    }
+            ),
+            @ApiResponse(responseCode = "404",
+                    description = "Vehicle not found",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "405",
+                    description = "Validation exception",
+                    content = @Content
+            ),
+            @ApiResponse(responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content
+            )}
+    )
+    @PutMapping(value = "/{id}", produces= {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<VehicleDto> updateVehicleById(@PathVariable("id") String id, @Valid @RequestBody VehicleDto vehicleDto) {
+        return null;
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "successful operation",
+                    content = {
+                            @Content
+                    }
+            ),
+            @ApiResponse(responseCode = "404",
+                    description = "Vehicle not found",
+                    content = @Content
+            )}
+    )
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteVehicleById(@PathVariable("id") String id) {
+        return null;
+    }
+
+}
---
+++ src/main/java/com/rest/vehicle/dto/VehicleDto.java
@@ -0,0 +1,27 @@
+package com.rest.vehicle.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VehicleDto{
+    private String id;
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
+    @NotBlank
+    private String name;
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
+    @NotBlank
+    private String vin;
+    @Pattern(regexp = "^[a-zA-Z0-9]+$")
+    @NotBlank
+    private String licensePlateNumber;
+    private Map<String, String> properties;
+}
